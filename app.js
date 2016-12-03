287a74d68395658b82345d9c43fb5b8b965f5886
Guillermo Tala <guille86598@gmail.com>
2016-12-03 16:12:34 +0000

    mas finales agregados

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ function Final(fecha, link, imagenLink, tema) {
 var app = angular.module('app', []);
 
 
-var listadoFinales = [
-    new Final(' 01/12/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-final-arquitectura-de-computadores-01-12-16-resuelto', 'http://www.utnianos.com.ar/foro/attachment.php?aid=14265'),
-    new Final('12/07/2016', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-12-07-16', 'http://www.utnianos.com.ar/foro/attachment.php?aid=14257'),
-    new Final('26/07/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-arquitectura-de-computadoras-final-26-07-2016', 'http://www.utnianos.com.ar/foro/attachment.php?aid=13708'),
-    new Final('06/10/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-final-de-arquitectura-06-10-16', ''),
-    new Final('01/03/2016', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadores-01-03-16', ''),
-    new Final('23/02/2016', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadores-23-02-2016', 'http://i65.tinypic.com/hx6olu.jpg'),
-    new Final('16/02/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-final-de-arquitectura-16-02-16-resuelto', 'http://www.utnianos.com.ar/foro/attachment.php?aid=12507')
-
-];
+var listadoFinales = getListadoFinales();
 
 app.controller('MainCtrl', function($scope, $window) {
     $scope.finales = listadoFinales;
---
+++ listadoFinales.js
@@ -0,0 +1,32 @@
+function getListadoFinales()
+{
+    
+    return [
+        new Final(' 01/12/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-final-arquitectura-de-computadores-01-12-16-resuelto', 'http://www.utnianos.com.ar/foro/attachment.php?aid=14265'),			
+new Final('12/07/2016', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-12-07-16', 'http://www.utnianos.com.ar/foro/attachment.php?aid=14257'),			
+new Final('26/07/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-arquitectura-de-computadoras-final-26-07-2016', 'http://www.utnianos.com.ar/foro/attachment.php?aid=13708'),			
+new Final('06/10/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-final-de-arquitectura-06-10-16', ''),			
+new Final('01/03/2016', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadores-01-03-16', ''),			
+new Final('23/02/2016', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadores-23-02-2016', 'http://i65.tinypic.com/hx6olu.jpg'),			
+new Final('16/02/2016', 'http://www.utnianos.com.ar/foro/tema-aporte-final-de-arquitectura-16-02-16-resuelto', 'http://www.utnianos.com.ar/foro/attachment.php?aid=12507'),			
+new Final('09/11/2015', 'http://www.utnianos.com.ar/foro/tema-aporte-final-arq-de-computadores-09-11-15', 'https://k33.kn3.net/1/A/5/7/F/7/DD4.jpg'),			
+new Final('02/12/2015', 'http://www.utnianos.com.ar/foro/tema-aporte-final-02-12-2015-arquitectura-de-computadoras', ''),			
+new Final('28/07/2015', 'http://www.utnianos.com.ar/foro/tema-aporte-final-arquitectura-de-computadore-28-07-2015', 'http://www.utnianos.com.ar/foro/attachment.php?aid=12742'),			
+new Final('16/02/2015', 'http://www.utnianos.com.ar/foro/tema-pedido-final-de-arquitectura-de-computadoras-16-12-2015', 'http://www.utnianos.com.ar/foro/attachment.php?aid=12084'),			
+new Final('24/02/2015', 'http://www.utnianos.com.ar/foro/tema-aporte-final-arquitectura-de-computadoras-24-02-2015-resuelto', 'https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xfp1/t31.0-8/10997303_10152784352256936_6083044882150380806_o.jpg'),			
+new Final('10/02/2015', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadoras-10-02-2015', 'http://i.imgur.com/LDZ3mfA.jpg'),			
+new Final('14/07/2015', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadoras-14-07-2015', 'http://www.utnianos.com.ar/foro/attachment.php?aid=11327'),			
+new Final('03/03/2015', 'http://www.utnianos.com.ar/foro/tema-final-de-arquitectura-del-3-3-2015-tema-2', ''),			
+new Final('16/07/2014', 'http://www.utnianos.com.ar/foro/tema-final-16-07-14-arquitectura-de-computadoras', ''),			
+new Final('30/07/2014', 'http://www.utnianos.com.ar/foro/tema-pedido-final-arquitectura-de-computadores-30-07-14', 'http://www.utnianos.com.ar/foro/attachment.php?aid=9331'),			
+new Final('10/12/2014', 'http://www.utnianos.com.ar/foro/tema-aporte-arquitectura-final-10-12-2014-resuelto-y-explicado', 'http://www.utnianos.com.ar/foro/attachment.php?aid=9988'),			
+new Final('20/02/2014', 'http://www.utnianos.com.ar/foro/tema-aporte-final-arquitectura-20-02-2014', 'http://www.utnianos.com.ar/foro/attachment.php?aid=8266'),			
+new Final('04/12/2013', 'http://www.utnianos.com.ar/foro/tema-aporte-finales-no-resueltos-de-arquitectura-de-computadores-2013-2014', 'http://www.utnianos.com.ar/foro/attachment.php?aid=9892'),			
+new Final('27/02/2013', 'http://www.utnianos.com.ar/foro/tema-aporte-finales-no-resueltos-de-arquitectura-de-computadores-2013-2014', 'http://www.utnianos.com.ar/foro/attachment.php?aid=9893'),			
+new Final('30/07/2013', 'http://www.utnianos.com.ar/foro/tema-aporte-finales-no-resueltos-de-arquitectura-de-computadores-2013-2014', 'http://www.utnianos.com.ar/foro/attachment.php?aid=9894'),			
+new Final('01/10/2014', 'http://www.utnianos.com.ar/foro/tema-final-arquitectura-de-computadoras-1-10-2014', 'http://www.utnianos.com.ar/foro/attachment.php?aid=7451'),			
+new Final('26/02/2014', 'http://www.utnianos.com.ar/foro/tema-26-02-2014-final-2-temas-resueltos-por-catedra', 'http://www.utnianos.com.ar/foro/attachment.php?aid=8364'),			
+new Final('26/02/2014', 'http://www.utnianos.com.ar/foro/tema-26-02-2014-final-2-temas-resueltos-por-catedra', 'http://www.utnianos.com.ar/foro/attachment.php?aid=8365'),			
+
+        ]
+}
